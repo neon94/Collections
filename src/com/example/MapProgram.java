db7c3012623f11c7f380f61dbf62f44bb51cb68a
Dawid Jaloza <dawidjaloza@gmail.com>
2020-05-14 14:44:24 +0200

    Map part 2

--- src/com/example/MapProgram.java
+++ src/com/example/MapProgram.java
@@ -16,7 +16,7 @@ public class MapProgram {
         languages.put("Python", "an interpreted, object-oriented, high-level programing language with dynamic semantics");
         languages.put("Algol", "an algorithmic language");
         System.out.println(languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code"));
-        System.out.println(languages.put("Lips", "Therein lies madness"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
 
 
         if(languages.containsKey("Java")) {
@@ -26,6 +26,21 @@ public class MapProgram {
         }
 
         System.out.println("=============");
+
+//        languages.remove("Lisp");
+        if(languages.remove("Algol", "an algorithmic language")) {
+            System.out.println("Algol removed");
+        } else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+
+        if(languages.replace("Lisp", "Therein lies madness", "a functional programing language with imperative features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+
+        System.out.println(languages.replace("Scala", "this will not be added"));
 
         for(String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
